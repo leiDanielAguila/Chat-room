5429798ea3fe92724668a7d3fc1274b31adea3be
Lei Aguila <aguila.christleidaniel@gmail.com>
2025-05-11 02:29:13 +0800

    added input text field

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-"use client"
+"use client";
 import Image from "next/image";
-import * as React from "react";
+import { useState } from "react";
 import { Box } from "@mui/material";
+import { TextField } from "@mui/material";
 
 type message = {
   id: number;
   text: string;
-}
+};
 
 type ChatBoxProps = {
   messageSent: message[];
-}
+};
 
 export function ChatBox(props: ChatBoxProps) {
+  // expects type message[]
   const message = props.messageSent;
   return (
-    <div className="w-full h-96 overflow-y-auto p-4 border border-pink-300 rounded-lg shadow-2xl p-4 bg-rose-200">
+    <div className="w-full h-96 overflow-y-auto p-4 mb-8 border border-pink-300 rounded-lg shadow-2xl bg-rose-200">
       {/* chat messages goes here */}
       {message.map((message) => (
         <div key={message.id} className="mb-2">
@@ -40,11 +42,18 @@ export function ChatBox(props: ChatBoxProps) {
 
 export default function Home() {
   const placeholder: message = { id: 1, text: "Hello, how are you?" };
-  const [messages, setMessages] = React.useState<message[]>([placeholder]);
+  const [messages, setMessages] = useState<message[]>([placeholder]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <ChatBox messageSent={messages}/>
-      <div></div>
+      <ChatBox messageSent={messages} />
+      <div className="flex flex-row items-center justify-between w-full p-4 border-pink-300 rounded-lg shadow-2xl bg-rose-200">
+        <TextField
+          id="outlined-basic"
+          label="Type your message here"
+          variant="outlined"
+          className="w-full mt-4"
+        />
+      </div>
     </div>
   );
 }
