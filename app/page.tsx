31d33ded22a58bcda57b8eaaaac505926cd50c82
Lei Aguila <aguila.christleidaniel@gmail.com>
2025-05-11 03:00:59 +0800

    currently works like a real chatbox but its static

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import { TextField } from "@mui/material";
+import SendRoundedIcon from "@mui/icons-material/SendRounded";
 
 type message = {
   id: number;
@@ -17,7 +18,7 @@ export function ChatBox(props: ChatBoxProps) {
   // expects type message[]
   const message = props.messageSent;
   return (
-    <div className="w-full h-96 overflow-y-auto p-4 mb-8 border border-pink-300 rounded-lg shadow-2xl bg-rose-200">
+    <div className="w-full h-96 overflow-y-auto p-4 mb-8 border border-zinc-700 rounded-lg shadow-2xl bg-stone-50">
       {/* chat messages goes here */}
       {message.map((message) => (
         <div key={message.id} className="mb-2">
@@ -43,16 +44,36 @@ export function ChatBox(props: ChatBoxProps) {
 export default function Home() {
   const placeholder: message = { id: 1, text: "Hello, how are you?" };
   const [messages, setMessages] = useState<message[]>([placeholder]);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  function handleSendMessage() {
+    const newMessage: message = {
+      id: messages.length + 1,
+      text: inputValue,
+    }
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setInputValue("");
+  }
+
+  useEffect(()=> {
+    
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <ChatBox messageSent={messages} />
-      <div className="flex flex-row items-center justify-between w-full p-4 border-pink-300 rounded-lg shadow-2xl bg-rose-200">
+      <div className="flex flex-row items-center justify-between w-full p-4 border border-zinc-700 rounded-lg shadow-2xl bg-white">
         <TextField
-          id="outlined-basic"
+          id="outlined-basic-textarea"
           label="Type your message here"
           variant="outlined"
           className="w-full mt-4"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
+        <div className="flex border border-zinc-700 rounded-lg ml-2 w-8 h-8 items-center justify-center" onClick={handleSendMessage}>
+          <SendRoundedIcon />
+        </div>
       </div>
     </div>
   );
